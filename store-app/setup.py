420e44486264f47a740d9b95a4b8377ef53e5d6d
Bohdan Parydubets <bogdanparidubec@gmail.com>
2023-02-23 16:20:44 +0200

    Added pylint

--- store-app/setup.py
+++ store-app/setup.py
@@ -1,18 +1,21 @@
+""" This is a setup module """
+
 from setuptools import setup, find_packages
 
 setup(
-    name='Store',
+    name='store',
     version='1.0.0.0',
     author='Bohdan Parydubets',
     description='An educational flask project',
-    long_description='An educational flask project with MySQL database. Created RESTful API and tests',
+    long_description='An educational flask project with'
+                     ' MySQL database. Created RESTful API and tests',
     url='https://github.com/Parydubets',
     python_requires='>=3.9, <4',
-    packages=find_packages(include=['Store']),
+    packages=find_packages(include=['store']),
     install_requires=[
         "Flask==2.2.3",
         "Jinja2==3.1.2",
         "Werkzeug==2.2.3",
+        "pylint==2.16.2",
     ],
-
 )
--- store-app/store/views.py
+++ store-app/store/views.py
@@ -1,27 +1,35 @@
-from Store import app
+""" This is the main routes module """
 from flask import render_template, url_for
+from store import app
+
 
 @app.route('/')
 @app.route('/clients')
 def clients():
+    """ The main (clients) page """
     return render_template('clients.html', page='client')
 
 @app.route('/orders')
 def orders():
+    """ The orders page """
     return render_template('orders.html', page='order')
 
 @app.route('/products')
 def products():
+    """ The products page """
     return render_template('products.html', page='product')
 
 @app.route('/new_client')
 def new_client():
+    """ Client creation page """
     return render_template('new_client.html', page='client')
 
 @app.route('/new_order')
 def new_order():
+    """ Order creation page """
     return render_template('new_order.html', page='order')
 
 @app.route('/new_product')
 def new_product():
+    """ Product creation page """
     return render_template('new_product.html', page='product')
