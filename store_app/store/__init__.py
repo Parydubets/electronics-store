df2f248a96b3385f253b06251a313737172fbb5a
Bohdan Parydubets <bogdanparidubec@gmail.com>
2023-02-26 14:13:47 +0200

    Added a few tests

--- store_app/store/__init__.py
+++ store_app/store/__init__.py
@@ -7,7 +7,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
             SECRET_KEY='somerandomkey',
-            DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+            DATABASE=os.path.join(app.instance_path, 'store.sqlite'),
     )
     if test_config is None:
             # load the instance config, if it exists, when not testing
@@ -22,8 +22,8 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    from . import views
-    app.register_blueprint(views.bp)
+    import store.views
+    app.register_blueprint(store.views.bp)
     @app.route("/hello")
     def hello():
         return "Hello, World!"
--- store_app/tests/test_views.py
+++ store_app/tests/test_views.py
@@ -9,3 +9,30 @@ def test_config():
 def test_hello(client):
     response = client.get('/hello')
     assert response.data == b'Hello, World!'
+
+def test_clients_route(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    response = client.get('/clients')
+    assert response.status_code == 200
+    #assert response.data.decode('utf-8') == 'Testing, Flask!'
+
+def test_orders_route(client):
+    response = client.get('/orders')
+    assert response.status_code == 200
+
+def test_products_route(client):
+    response = client.get('/products')
+    assert response.status_code == 200
+
+def test_new_client_route(client):
+    response = client.get('/new_client')
+    assert response.status_code == 200
+
+def test_new_order_route(client):
+    response = client.get('/new_order')
+    assert response.status_code == 200
+
+def test_new_product_route(client):
+    response = client.get('/new_product')
+    assert response.status_code == 200
