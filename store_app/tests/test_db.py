401ee12d0ee6e5523f25db599226be44c3128ea1
Parydubets <97911094+Parydubets@users.noreply.github.com>
2023-03-01 14:20:28 +0200

    Update test_db.py

--- store_app/tests/test_db.py
+++ store_app/tests/test_db.py
@@ -2,4 +2,4 @@ from store import create_app
 from sqlalchemy_utils import database_exists
 
 def test_db_cnnection():
-    assert database_exists('mysql://root:admin@localhost:3306/store')
+    assert database_exists('mysql://root@127.0.0.1:3306/store')
