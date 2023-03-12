85cde41c2420344108453984ce9bdd4124826c3b
Bohdan Parydubets <bogdanparidubec@gmail.com>
2023-03-12 10:34:00 +0200

    Adding more tests

--- store_app/tests/test_db.py
+++ store_app/tests/test_db.py
@@ -7,7 +7,7 @@ def test_db_cnnection():
     db_user = str(db_user)+':'
     db_password = os.environ.get('DB_PASSWORD')
     db_host = os.environ.get('DB_HOST')
-    if db_password is '':
+    if db_password == '':
         db_user=db_user[0:-1]
     assert database_exists('mysql://{}{}@{}/store'.format(db_user,db_password,db_host))
     #assert database_exists('mysql://root:admin@localhost:3306/store')
