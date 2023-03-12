8caf6be21e4e4120c94c6c945fcdc265d4cea8f5
Bohdan Parydubets <bogdanparidubec@gmail.com>
2023-03-12 19:39:04 +0200

    Adding more tests

--- store_app/tests/conftest.py
+++ store_app/tests/conftest.py
@@ -18,15 +18,3 @@ def client(app):
     """ App test client """
     return app.test_client()
 
-
-@pytest.fixture()
-def runner(app):
-    """ Initiate cli_runner """
-    return app.test_cli_runner()
-
-
-def test_hello_command(runner):
-    """ Hello test """
-    result = runner.invoke(args="seed")
-    print(result.output)
-    assert "successfully" in result.output
